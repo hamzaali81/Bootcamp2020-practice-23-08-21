499849599c6dc87702f501dcdad1a2626fcd0420
hamzaali <hamza54834@gmail.com>
2021-09-14 00:51:37 +0500

    App: Complete

---
+++ Typescript/Quiz-App-Typescript/src/Api/api.tsx
@@ -0,0 +1,32 @@
+import { suffleArray } from "../util/util";
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "HARD"
+}
+
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string; 
+    type: string;
+} 
+
+export type QuestionState = Question & { answers: string[] }; 
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: suffleArray([...question.incorrect_answers, question.correct_answer])
+        }
+    ));
+    
+}
